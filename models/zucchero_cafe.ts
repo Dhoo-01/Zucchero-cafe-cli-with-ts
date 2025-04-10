e63fd2bd57765ad75107d26d56450406dbfbe53b
riiiiy <justateammates@gmail.com>
2025-04-10 15:28:02 +0800

    feat: add dipscount feature

--- models/zucchero_cafe.ts
+++ models/zucchero_cafe.ts
@@ -6,6 +6,13 @@ class ZuccheroCafe {
     console.log(`Hello ${customerName}, I'm ${centaureissi.name} and I work as a ${centaureissi.job} here. What would you like today?`)
   }
   generateReceipt(user: Customer, totalPrice: number, amount: number) {
+    const hasDiscount = totalPrice > 50
+    const finalPrice = hasDiscount ? totalPrice - totalPrice * 0.08 : totalPrice
+    const change = amount - finalPrice
+    if (totalPrice > 50) {
+      console.log("you get discount 8% because your total price was higher than 50")
+    }
+
     const receipt = `
     ===== Zucchero Cafe Receipt =====
     Customer: ${user.name}
@@ -13,14 +20,14 @@ class ZuccheroCafe {
     ---------------------------------
     Orders: ${user.orders.map((item, i) => `|${i + 1}. ${item.name.padEnd(10)} price: ${item.price}`
     ).join("\n")}
-
-    Total: ${totalPrice}
+    
+    Total: ${Math.round(finalPrice)}
     Payment: ${user.pay(amount)}
-    Change: ${amount - totalPrice}
+    Change: ${Math.round(change)}
     ---------------------------------
     Thank You for visiting!
     `
     console.log(receipt)
-  }
+  } 
 }
-export { ZuccheroCafe }
+export { ZuccheroCafe } 
